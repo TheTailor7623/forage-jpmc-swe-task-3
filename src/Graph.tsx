bc9cfb90acd9ae765a59d6afb7f6580416e24b31
TheTailor7623 <yseyam7623@gmail.com>
2024-08-23 12:52:33 +0100

    Solution to task3

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,58 +1,92 @@
-import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
-import { ServerRespond } from './DataStreamer';
-import { DataManipulator } from './DataManipulator';
-import './Graph.css';
+import React, { Component } from "react"; //Importing the function component from react as react
+import { Table, TableData } from "@finos/perspective"; //Importing the function table from @finos/perspective
+import { ServerRespond } from "./DataStreamer"; //Importing the serverrespong function from Datastreamer
+import { DataManipulator } from "./DataManipulator"; //Importing DataManipulator from DataManipulator
+import "./Graph.css"; //Importing graph.css file
 
+//Creating an interface called IProps
 interface IProps {
-  data: ServerRespond[],
+  //Any object implementing the interface IProps must have a property called data
+  data: ServerRespond[];
 }
 
 interface PerspectiveViewerElement extends HTMLElement {
-  load: (table: Table) => void,
+  load: (table: Table) => void;
 }
+//Creating a graph class that is implementing the interface IProps
 class Graph extends Component<IProps, {}> {
+  //Any object belonging to the graph class has a table property which is a Table with undefined varible types
   table: Table | undefined;
 
+  //This is the render method within the graph class which allows any object within this class to be able to render the graph
   render() {
-    return React.createElement('perspective-viewer');
+    //Rendering the grah using perspective-viewer when function is called
+    return React.createElement("perspective-viewer");
   }
 
+  //This function called componentsDidMount and is used to configure the graph to be rendered
   componentDidMount() {
-    // Get element from the DOM.
-    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    // Get element from the DOM. ***
+    const elem = (document.getElementsByTagName(
+      "perspective-viewer"
+    )[0] as unknown) as PerspectiveViewerElement;
 
+    //This is the schema that the table will have and the datatype of each
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
+      timestamp: "date",
+
+      //MY CODE
+      ratio: "float",
+      upper_bound: "float",
+      lower_bound: "float",
+      trigger_alert: "float",
+      price_abc: "float",
+      price_def: "float",
+      //MY CODE
     };
 
+    //Setting the schema to the table based on a conditional if statement
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
     }
+
+    //if the table exists then...
     if (this.table) {
-      // Load the `table` in the `<perspective-viewer>` DOM reference.
+      // Load the `table` in the `<perspective-viewer>` DOM reference. ***
       elem.load(this.table);
-      elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
-      elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
-      elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
-        timestamp: 'distinct count',
-      }));
+      elem.setAttribute("view", "y_line");
+      elem.setAttribute("row-pivots", '["timestamp"]');
+
+      //MY CODE
+      elem.setAttribute(
+        "columns",
+        '["ratio", "upper_bound", "lower_bound", "trigger_alert"]'
+      );
+
+      elem.setAttribute(
+        "aggregates",
+        JSON.stringify({
+          timestamp: "distinct count",
+          ratio: "avg",
+          upper_bound: "avg",
+          lower_bound: "avg",
+          trigger_alert: "avg",
+          price_abc: "avg",
+          price_def: "avg",
+        })
+      );
+      //MY CODE
     }
   }
 
+  //Function used to update the table when needed
   componentDidUpdate() {
+    //Conditional if statement used to check if the table exists
     if (this.table) {
-      this.table.update(
+      //If it does then the table is updated using the props method to the IProps interface which in turn has the data property which contains an array of data from the server
+      this.table.update(([
         DataManipulator.generateRow(this.props.data),
-      );
+      ] as unknown) as TableData);
     }
   }
 }
